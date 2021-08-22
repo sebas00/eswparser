7ed8ce8d27d0b4609e2b609295c8d03c6c2aa1a5
sebas00 <sebastiaan@gmail.com>
2021-08-22 13:17:57 +0200

    heroku

--- server.js
+++ server.js
@@ -61,11 +61,7 @@ fastify.post("/", function (req, res) {
 });
 
 // Run the server and report out to the logs
-fastify.listen(process.env.PORT, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
-});
+fastify.listen(process.env.PORT || 3000, err => {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
